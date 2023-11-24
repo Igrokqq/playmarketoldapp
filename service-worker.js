203f1f6fb7f589578ef4004140aea0191c4252e9
p.gritsenko <p.yakun@evnedev.com>
2023-11-24 18:42:05 +0200

    i

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+// service-worker.js
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('my-cache').then(cache => {
+      return cache.addAll([
+        '/', // Cache the root URL
+        '/index.html', // Add other URLs you want to cache
+        // '/path/to/other-resources', // Add other paths you want to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => {
+          // Delete old caches if needed
+        }).map(cacheName => {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
+
+
